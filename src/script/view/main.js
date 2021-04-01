2befb1b6813cff5ed3203317cf79b86e75ba9499
Cah Bantul <fardan.nozami@gmail.com>
2021-04-01 13:50:05 +0700

    menerapkan destructuring Object pada main.js

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -11,9 +11,9 @@ const main = function () {
   const renderResult = function (results) {
     clubListElement.innerHTML = "";
     results.forEach(function (club) {
-      const name = club.name;
-      const fanArt = club.fanArt;
-      const description = club.description;
+      const { name, fanArt, description } = club;
+      //   const fanArt = club.fanArt;
+      //   const description = club.description;
 
       const clubElement = document.createElement("div");
       clubElement.setAttribute("class", "club");
